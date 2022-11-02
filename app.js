ebf4800115f416af0771f45a9d3d95b82d4e41eb
KariNNa33 <103944686+KariNNa33@users.noreply.github.com>
2022-11-02 11:55:56 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const host = '127.0.0.1'
+const port = 7000
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+    res.render('home')
+  })
+
+app.listen(port, host, function () {
+  console.log(`Server listens http://${host}:${port}`)
+})
+
